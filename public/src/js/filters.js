e2e119579c112e7d37a30d05b023864b3e2c8808
Codeb Fan <codeb2cc@gmail.com>
2013-10-01 04:48:37 +0800

    Add angular filter `second2time`, `unitResize`.

--- public/src/js/filters.js
+++ public/src/js/filters.js
@@ -2,4 +2,31 @@
 
 "use strict";
 
-angular.module('gmcadmin.filters', [])
+angular.module('gmcadmin.filters', [
+]).filter('second2time', function () {
+  return function (value, flag) {
+    var days = Math.floor(value / 60 / 60 / 24)
+    var hours = Math.floor(value / 60 / 60 % 24)
+    var mins = Math.floor(value / 60 % 60)
+
+    var timeStr = null
+    if (days + hours + mins === 0) {
+      timeStr = value + ' secs'
+    } else {
+      timeStr = days + ' days ' + hours + ' hrs ' + mins + ' mins'
+    }
+
+    return timeStr
+  }
+}).filter('unitResize', function () {
+  var units = ['', 'K', 'M', 'G', 'T']
+  return function (value, flag) {
+    var i = 0
+    var base = flag ? flag : 1024
+    for (; i < units.length; i++) {
+      if (value < base) { break }
+      value /= base
+    }
+    return value.toFixed(1) + ' ' + units[i]
+  }
+})
