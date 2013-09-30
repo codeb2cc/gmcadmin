e55f521a1812fec68cf58a0c673ffac85b12ecca
Codeb Fan <codeb2cc@gmail.com>
2013-09-30 14:01:44 +0800

    Use WebSocket to pull memcached server stats.

---
+++ app/controllers/websocket.go
@@ -0,0 +1,107 @@
+package controllers
+
+import (
+    "net"
+    "sort"
+    "encoding/json"
+    "code.google.com/p/go.net/websocket"
+    "github.com/robfig/revel"
+    "github.com/codeb2cc/gomemcache/memcache"
+)
+
+type WebSocket struct {
+    *revel.Controller
+}
+
+type Message struct {
+    Status string
+    Cmd string
+    Data interface{}
+}
+
+var (
+    cmdServerStats = "server"
+    cmdSettingsStats = "settings"
+    cmdSlabStats = "slabs"
+)
+
+func (c WebSocket) Socket(host string, ws *websocket.Conn) revel.Result {
+    mcServer := "127.0.0.1:11211"
+    mcClient := memcache.New(mcServer)
+    addr, err := net.ResolveTCPAddr("tcp", mcServer)
+
+    if err != nil {
+        revel.ERROR.Printf("Cannot resolve memcached address %s", mcServer)
+        return nil
+    }
+
+    // Stuff websocket events into a channel
+    message := make(chan string)
+    go func() {
+        var msg string
+        for {
+            err := websocket.Message.Receive(ws, &msg)
+            if err != nil {
+                close(message)
+                return
+            }
+            message <- msg
+        }
+    }()
+
+    // Listen
+    for {
+        msg, ok := <-message
+        if !ok {
+            return nil
+        }
+
+        response := Message{"error", "", nil}
+        switch msg {
+        case cmdServerStats:
+            response.Cmd = cmdServerStats
+            if stats, err := mcClient.Stats(addr); err == nil {
+                response.Status = "success"
+                response.Data = stats
+            }
+        case cmdSettingsStats:
+            response.Cmd = cmdSettingsStats
+            if stats, err := mcClient.StatsSettings(addr); err == nil {
+                response.Status = "success"
+                response.Data = stats
+            }
+        case cmdSlabStats:
+            response.Cmd = cmdSlabStats
+            if stats, err := mcClient.StatsSlabs(addr); err == nil {
+                var keys []int
+                var values []interface{}
+                for key := range stats {
+                    keys = append(keys, key)
+                }
+                sort.Ints(keys)
+                for _, key := range keys {
+                    values = append(values, stats[key])
+                }
+                response.Status = "success"
+                response.Data = values
+            }
+        default:
+            revel.INFO.Print("Receive unknown message: ", msg)
+        }
+
+        jsonBytes, err := json.Marshal(response)
+        if err != nil {
+            revel.WARN.Print(err)
+            return nil
+        }
+
+        if websocket.Message.Send(ws, string(jsonBytes)) != nil {
+            // Disconnected
+            revel.INFO.Print("Send message failed. Disconnect.")
+            return nil
+        }
+    }
+
+    return nil
+}
+
--- public/src/js/controllers.js
+++ public/src/js/controllers.js
@@ -5,21 +5,79 @@
 angular.module('gmcadmin.controllers', [
 ]).controller('NavCtrl', [
   '$scope'
-, '$route'
 , '$location'
-, function ($scope) {
-    console.log('NavCtrl')
+, function ($scope, $location) {
+    $scope.navClass = function (nav) {
+      return $location.path() == nav ? 'active' : ''
+    }
   }
 ])
 .controller('ServerCtrl', [
   '$scope'
-, function ($scope) {
-    console.log('ServerCtrl')
+, '$timeout'
+, function ($scope, $timeout) {
+    $scope.socket = null
+    $scope.connected = false
+
+    $scope.serverStats = {}
+    $scope.settingsStats = {}
+
+    var socketInit = function () {
+      $scope.socket = new WebSocket('ws://' + location.host + ':8000/ws/socket')
+      $scope.socket.onopen = function (evt) {
+        $scope.$apply(function () {
+          $scope.connected = true
+          $scope.update()
+        })
+      }
+      $scope.socket.onclose = function (evt) {
+        $scope.$apply(function () {
+          $scope.connected = false
+        })
+      }
+      $scope.socket.onmessage = function (evt) {
+        $scope.$apply(function () {
+          try {
+            var data = JSON.parse(evt.data)
+            switch (data.Cmd) {
+              case 'server':
+                $scope.serverStats = data.Data
+                break
+              case 'settings':
+                $scope.settingsStats = data.Data
+                break
+            }
+          } catch (exc) {
+            console.log(exc)
+          }
+        })
+      }
+    }
+
+    $scope.update = function () {
+      if ($scope.connected) {
+        $scope.socket.send('server')
+        $scope.socket.send('settings')
+      }
+    }
+    $scope.updateInterval = setInterval(function () {
+      $scope.update()
+    }, 60 * 1000)
+
+    $scope.$on('$routeChangeStart', function () {
+      if ($scope.connected) {
+        $scope.socket.close()
+      }
+    })
+
+    $timeout(function () {
+      socketInit()
+    }, 0)
+
   }
 ])
 .controller('SlabCtrl', [
   '$scope'
 , function ($scope) {
-    console.log('SlabCtrl')
   }
 ])
